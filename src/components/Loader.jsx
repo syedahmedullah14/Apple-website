6e8150d1fbcdb0e60ef36cfec762283031aa810a
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-06-27 16:13:20 +0530

    Developed Loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,15 @@
+import { Html } from '@react-three/drei'
 import React from 'react'
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <Html>
+      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
+        <div className="w-[10vw] h-[10vw] rounded-full">
+          Loading...
+        </div>
+      </div>
+    </Html>
   )
 }
 
