13e2f1fe0cce14bf6f1662e288c593417bb4eb17
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-06-27 14:33:51 +0530

    Intialized components

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ const App = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+  <main className='bg-black'>
+    navbar
+    hero
+    highlights
+  </main>
   )
 }
 
