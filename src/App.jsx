ba65b4ea1421b405a86d38a4766f91a78f4f7c05
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-06-27 16:23:03 +0530

    Imported Components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
 import Model from './components/Model'
+import Features from './components/Features'
+import HowItWorks from './components/HowItWorks'
+import Footer from './components/Footer'
 
 const App = () => {
   const [count, setCount] = useState(0)
@@ -14,6 +17,9 @@ const App = () => {
     <Hero />
     <Highlights />
     <Model />
+    <Features />
+    <HowItWorks />
+    <Footer />
   </main>
   )
 }
